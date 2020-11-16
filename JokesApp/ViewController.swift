99793ee8fa9145d99ef088fb020f3a9057f142ce
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-16 11:58:31 -0800

    Initial Commit

---
+++ JokesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JokesApp
+//
+//  Created by Angelina Tsuboi on 11/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
