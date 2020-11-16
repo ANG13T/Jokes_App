4dc0bd4de5637e7a45a6d5e17dce48e2a3790f91
angelina-tsuboi <angelina.t1832@gmail.com>
2020-11-16 12:50:14 -0800

    got jokes to work

--- JokesApp/ViewController.swift
+++ JokesApp/ViewController.swift
@@ -13,7 +13,25 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let urlString = "https://sv443.net/jokeapi/v2/joke/Dark?amount=5"
+        print(urlString)
         
+        if let url = URL(string: urlString){
+            if let data = try? Data(contentsOf: url){
+                parse(json: data)
+            }
+        }
+    }
+    
+    func parse(json: Data){
+        let decoder = JSONDecoder()
+        print("parsing JSON")
+        print(json.isEmpty)
+        if let jsonJokes = try? decoder.decode(Jokes.self, from: json){
+            jokes = jsonJokes.jokes
+            print(jokes)
+            tableView.reloadData()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -21,9 +39,10 @@ class ViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let joke = jokes[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Joke", for: indexPath)
-        cell.textLabel?.text = "Title"
-        cell.detailTextLabel?.text = "Description"
+        cell.textLabel?.text = joke.setup
+        cell.detailTextLabel?.text = joke.delivery
         return cell
     }
 
